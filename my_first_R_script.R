ff053f9bd091d49668e0e896e0eef8c626c6b841
Leroy Yeo <leroy_yeo@hotmail.com>
2020-05-13 14:54:43 -0400

    inital commit for version control

---
+++ my_first_R_script.R
@@ -0,0 +1,17 @@
+1+1
+
+1+1
+1+1
+1+1
+1+1
+example <- matrix(c(1,2,3,4,5,6,7,8), nrow = 4, ncol =2)1+1
+
+example <- matrix(c(1,2,3,4,5,6,7,8), nrow = 4, ncol =2)
+installed.packages(c("ggplot2","devtools","lem4"))
+install.packages("ggplot2")
+load(ggplot2)
+install.packages("devtools")
+library(devtools)
+install.packages("KernSmooth")
+install.packages("Rtools")
+help(package = devtools)
